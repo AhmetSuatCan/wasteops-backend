2f95f3443840d81e1b16c9f696fdb555ee92295d
Ahmet Suat CAN <suatahmet@hotmail.com>
2025-04-06 03:43:57 +0300

    Added JWT token verification and refresing endpoints and api versioning

--- wasteops_backend/config/urls.py
+++ wasteops_backend/config/urls.py
@@ -22,8 +22,8 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="WasteOps API",
-        default_version="v1",
+        title="Your API",
+        default_version='v1',
         description="API documentation",
     ),
     public=True,
@@ -31,9 +31,18 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    # Admin and DRF auth
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    re_path(r"^swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
-    re_path(r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-    path('users/', include('apps.users.urls'))
+
+    # Documentation
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    # API Version 1
+    path('api/v1/', include([
+        # Users app
+        path('users/', include('apps.users.urls')),
+
+    ])),
 ]
